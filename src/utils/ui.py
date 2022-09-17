c92b54c769a2f7bd48ca71ba362d19e9f794a5b5
Hanz <hanz@godot.id>
2022-09-17 18:02:49 +0700

    fix(utils/ui): Fixed mispositioned argument of ljust

--- src/utils/ui.py
+++ src/utils/ui.py
@@ -11,5 +11,5 @@ def center(s):
 def middle(offset):
 	print(term.move_y(term.height // 2 + offset), end="")
 
-def progress_bar(on='#', off='-', maxval=10, value=3):
-	return (on * value).ljust(off, maxval)
+def progress_bar(on=35, off=45, maxval=10, value=3):
+	return (chr(on) * value).ljust(maxval, chr(off))
