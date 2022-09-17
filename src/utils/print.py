cce734fd2ab3b7a35e2b2f90943e4927ebd6d28b
Hanz <hanz@godot.id>
2022-09-17 20:30:40 +0700

    feat(utils/print): Added log system

--- src/utils/print.py
+++ src/utils/print.py
@@ -1,8 +1,19 @@
+logs = list()
 
 '''
 Extends Python's print
 to print and rturn values
 '''
 def vprint(s, *arg, **kwargs):
-	print(s.format(*arg), **kwargs)
+	if not kwargs.get('log', False):
+		print(s.format(*arg), **kwargs)
+	else:
+		log(s.format(*arg), **kwargs)
 	return arg
+
+def log(s, **kwargs):
+	logs.append((s, kwargs))
+
+def flush_logs():
+	for s, kw in logs:
+		print(s, **kw)
