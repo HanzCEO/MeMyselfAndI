8a86b5d95eb9b4aa50097d6488c46aa15688a986
Hanz <hanz@godot.id>
2022-09-17 17:18:51 +0700

    feat(utils): Added ui.py

---
+++ src/utils/ui.py
@@ -0,0 +1,15 @@
+from blessed import Terminal
+
+term = Terminal()
+
+def clear():
+	print(term.home + term.clear, end="")
+
+def center(s):
+	print(term.center(s))
+
+def middle(offset):
+	print(term.move_y(term..height // 2 + offset), end="")
+
+def progress_bar(on='#', off='-', maxval=10, value=3):
+	return (on * value).ljust(off, maxval)
